86d55a339f4371b7348f3ef59979694132505391
mlovewt <wtchxx-223@163.com>
2016-12-30 09:55:07 +0800

    commit1_a_1

---
+++ src/com/winterbe/git/GitCommitShowCase.java
@@ -0,0 +1,11 @@
+package com.winterbe.git;
+
+/**
+ * 类名     :GitCommitShowCase
+ * 项目名称 :java8-tutorial
+ * 创建时间 :2016/12/30
+ * 作     者:sd-wangtaicheng@sdcncsi.com.cn
+ * 版     本:v1
+ */
+public class GitCommitShowCase {
+}
